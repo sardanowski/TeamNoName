1e4c5c7cbd6cb75ea2db2e488972c25b96e540a3
bigar <bigar@141.219.245.74>
2018-10-16 13:47:46 -0400

    Made a rectangle to represent blizzard

---
+++ core/src/com/mygdx/game/Character.java
@@ -0,0 +1,19 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.math.Rectangle;
+
+/*
+ * Character class
+ */
+public class Character {
+	void create() {
+	Rectangle Blizzard;
+	Blizzard = new Rectangle();
+	
+	Blizzard.width = 64;
+	Blizzard.height = 64;
+	
+	}
+	
+
+}

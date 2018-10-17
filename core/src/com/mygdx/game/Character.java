0ec6f5f5e644a7d4380e248eabc4f82790321c59
bigar <bigar@141.219.245.74>
2018-10-17 12:36:47 -0400

    Created character class and movement

--- core/src/com/mygdx/game/Character.java
+++ core/src/com/mygdx/game/Character.java
@@ -1,19 +1,34 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.math.Rectangle;
 
 /*
  * Character class
  */
 public class Character {
+	
+
+	private float x;
+
 	void create() {
 	Rectangle Blizzard;
 	Blizzard = new Rectangle();
-	
 	Blizzard.width = 64;
 	Blizzard.height = 64;
+	Blizzard.x = 400;
+	Blizzard.y = 20;
 	
 	}
 	
+	void Movement() {
+	Character Blizzard = new Character();
+
+	if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+		Blizzard.x -= 200 * Gdx.graphics.getDeltaTime();
+	if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+		Blizzard.x += 200* Gdx.graphics.getDeltaTime();
+	}
 
 }

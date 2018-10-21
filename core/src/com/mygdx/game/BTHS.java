5ca413395230ef505e1298ece173ae224bfcbf7b
Ohbober <43010869+Ohbober@users.noreply.github.com>
2018-10-21 16:12:21 -0400

    Graphics patch to BTHS
    Supposed to load up the tmx and the level but wont find the file in assets, be sure to set build path in the desktop with assets

--- core/src/com/mygdx/game/BTHS.java
+++ core/src/com/mygdx/game/BTHS.java
@@ -3,23 +3,42 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class BTHS extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	TiledMap level1;
+	OrthographicCamera camera;
+	TiledMapRenderer tiledMapRenderer;
 	
 	@Override
 	public void create () {
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight(); 
+		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,w,h);
+		camera.update();
+		level1 = new TmxMapLoader().load("1-1v2.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(level1);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("blizzRight.png");
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();

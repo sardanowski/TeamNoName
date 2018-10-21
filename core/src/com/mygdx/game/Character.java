8dd0ee736c371eb69483560764d9bc7d68033344
bigar <bigar@Stephen-Laptop>
2018-10-21 13:42:52 -0400

    Added variables for rendering, didn't get a chance to finish

--- core/src/com/mygdx/game/Character.java
+++ core/src/com/mygdx/game/Character.java
@@ -2,33 +2,45 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
 
 /*
  * Character class
  */
 public class Character {
-	
+	//variables for textures and music
+	private Texture Blizzard;
+	private Texture stage;
+	private Music stageMusic;
+
 
 	private float x;
 
 	void create() {
-	Rectangle Blizzard;
-	Blizzard = new Rectangle();
-	Blizzard.width = 64;
-	Blizzard.height = 64;
-	Blizzard.x = 400;
-	Blizzard.y = 20;
-	
-	}
-	
-	void Movement() {
-	Character Blizzard = new Character();
+		//TODO: Load character, map, and sound files.
+		//I've been using this as a reference: https://github.com/libgdx/libgdx/wiki/A-simple-game
+		
 
-	if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
-		Blizzard.x -= 200 * Gdx.graphics.getDeltaTime();
-	if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
-		Blizzard.x += 200* Gdx.graphics.getDeltaTime();
+		Rectangle Blizzard;
+		Blizzard = new Rectangle();
+		Blizzard.width = 64;
+		Blizzard.height = 64;
+		Blizzard.x = 400;
+		Blizzard.y = 20;
+
+	}
+
+
+
+	void Movement() {
+		Character Blizzard = new Character();
+
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+			Blizzard.x -= 200 * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+			Blizzard.x += 200* Gdx.graphics.getDeltaTime();
 	}
 
 }
